9c404f6bfd80684b5c39376431e8985b4a78dccf
zereight <zereight@zereightui-MacBookPro.local>
2020-09-21 21:33:03 +0900

    createBlock

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,25 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var CryptoJS = __importStar(require("crypto-js"));
 var Block = /** @class */ (function () {
     function Block(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -7,8 +28,23 @@ var Block = /** @class */ (function () {
         this.data = data;
         this.timestamp = timestamp;
     }
+    Block.calculateBlockHash = function (index, previousHash, timestamp, data) {
+        return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    };
     return Block;
 }());
 var generateBlock = new Block(0, "1234", "123", "Hi", 2020);
 var blockChain = [generateBlock];
-console.log(blockChain);
+var getBlockchain = function () { return blockChain; };
+var getLastestBlock = function () { return blockChain[blockChain.length - 1]; };
+var getNewTimeStamp = function () { return Math.round(new Date().getTime() / 1000); };
+var createNewBlock = function (data) {
+    var previousBlock = getLastestBlock();
+    var newIndex = previousBlock.index + 1;
+    var newTimestamp = getNewTimeStamp();
+    var newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    var newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ) => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
+
   constructor(
     index: number,
     hash: string,
@@ -21,6 +33,32 @@ class Block {
 
 const generateBlock: Block = new Block(0, "1234", "123", "Hi", 2020);
 
-const blockChain: [Block] = [generateBlock];
+const blockChain: Block[] = [generateBlock];
 
-console.log(blockChain);
+const getBlockchain = (): Block[] => blockChain;
+const getLastestBlock = (): Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLastestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
