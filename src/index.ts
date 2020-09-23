2470652d4fc82e79f2fac1c487849af2908bc265
zereight <zereight@zereightui-MacBookPro.local>
2020-09-23 21:41:57 +0900

    Validate Block

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,19 @@ const createNewBlock = (data: string): Block => {
     data,
     newTimestamp
   );
+
+  addBlock(newBlock);
+
   return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string => {
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
 };
 
 const isBlockValid = (candidateBlock: Block, previousBlock: Block) => {
@@ -77,5 +89,15 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block) => {
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLastestBlock())) {
+    blockChain.push(candidateBlock);
   }
 };
